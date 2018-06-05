bc89df97c862b965bc99dbe2d48d0ba56f586137
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-05 16:26:59 -0700

    configured entry file to update root

--- frontend/index.jsx
+++ frontend/index.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as APIUtil from './util/session_api_util';
 import configureStore from './store/store';
+import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
   // window.logout = APIUtil.logout;
   // window.login = APIUtil.login;
   // window.signup = APIUtil.signup;
   const store = configureStore();
-  window.getState = store.getState;
-  window.dispatch = store.dispatch;
+  // window.getState = store.getState;
+  // window.dispatch = store.dispatch;
+
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>360 px</h1>, root);
+  ReactDOM.render(<Root store={store}/>, root);
 });
