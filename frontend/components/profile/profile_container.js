fb3b7f25fdd09c3759d4c7c246bb95a38b2a350d
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-07 14:03:23 -0700

    pushing to heroku

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -1,12 +1,12 @@
 import { connect } from 'react-redux';
-import { logout } from '../../actions/session_actions';
-import Profile from './profile_page';
+import ProfilePage from './profile_page';
 
 const mapStateToProps = ({entities, session}) => ({
   currentUser: entities.users[session.id]
 });
 
 const mapDispatchToProps = () => dispatch => ({
+
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(Profile);
+export default connect(mapStateToProps, null)(ProfilePage);
--- frontend/components/profile/profile_page.jsx
+++ frontend/components/profile/profile_page.jsx
@@ -1,18 +1,18 @@
-import React from 'React';
-import NavBarContainer from '../nav/nav_container';
+import React from 'react';
+import NavContainer from '../nav/nav_container';
 
-class Profile extends React.Component {
+class ProfilePage extends React.Component {
   constructor(props){
     super(props);
   }
   render() {
     return (
       <div className="profile-holder">
-        <NavBarContainer />
-        <h1>Hello {this.props.currentUser.username}. This is your profile page.</h1>
+        <NavContainer />
+        <h1>Hello {this.props.currentUser.username}. {"This is your profile page."}</h1>
       </div>
     );
   }
 }
 
-export default Profile;
+export default ProfilePage;
