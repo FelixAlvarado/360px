0d7889f0ad41c839be6c7a97429287207e1ed4fb
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-07 00:48:18 -0700

    modifyed routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:create,:update]
+    resources :users
     resource :session, only: [:create, :destroy]
   end
 
   root 'static_pages#root'
 end
+
+# , only: [:create,:update]
