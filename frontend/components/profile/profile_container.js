c50a4d649ca0d6cbf2eff58c6300ece17d73d3f0
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-07 13:07:50 -0700

    modified file names

--- frontend/components/profile/profile_container.js
+++ frontend/components/profile/profile_container.js
@@ -1,6 +1,6 @@
 import { connect } from 'react-redux';
 import { logout } from '../../actions/session_actions';
-import Profile from './profile';
+import Profile from './profile_page';
 
 const mapStateToProps = ({entities, session}) => ({
   currentUser: entities.users[session.id]
