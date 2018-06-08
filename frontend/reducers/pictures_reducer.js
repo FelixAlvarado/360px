1a6312808356af58e13b6496835e1a062c49c950
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-07 18:05:39 -0700

    finished pictures reducer

--- frontend/reducers/pictures_reducer.js
+++ frontend/reducers/pictures_reducer.js
@@ -1,8 +1,16 @@
 import {RECEIVE_PICTURE, RECEIVE_PICTURES} from '../actions/picture_actions';
-// 
-// const PicturesReducer = (initialState = {}, action) => {
-//   Object.freeze(initialState);
-//   // switch (action.type){
-//   //   case
-//   // }
-// };
+import merge from 'lodash/merge';
+
+const picturesReducer = (initialState = {}, action) => {
+  Object.freeze(initialState);
+  switch (action.type){
+    case RECEIVE_PICTURE:
+    return merge(initialState, {[action.picture.id]:action.picture});
+    case RECEIVE_PICTURES:
+    return merge({}, action.pictures);
+    default:
+    return initialState;
+  }
+};
+
+export default picturesReducer;
