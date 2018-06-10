546958142caccae74b7071eef45d404f7a318326
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-09 18:21:35 -0700

    finsihed show button css

--- frontend/components/show/show_picture.jsx
+++ frontend/components/show/show_picture.jsx
@@ -14,13 +14,17 @@ class ShowPictureComponent extends React.Component {
     return (
       <div className="show-div">
 
-          <div className="show-child-1"><img className='show-image' src={`${this.props.picture.image_url}`}/></div>
-        <div c
-          lassName="show-child-2">
-          <p>  <img className = "image-placeholder" src="https://s15.postimg.cc/8agnin31n/seth_curry.jpg" /><span className="show-username"><Link className={`show-link ${this.nameLink()}`} to={`/profile/${this.props.user.id}`} >{this.props.user.username}</Link></span></p>
-          <h2 className="show-title">{this.props.picture.title}</h2>
-        </div>
+        <div className="show-child-1"><img className='show-image' src={`${this.props.picture.image_url}`}/></div>
+          <div className="show-child-2">
+            <p> <img className = "image-placeholder" src="https://s15.postimg.cc/8agnin31n/seth_curry.jpg" />
+            <span className="show-username"><Link className={`show-link ${this.nameLink()}`} to={`/profile/${this.props.user.id}`} >{this.props.user.username}</Link></span></p>
+            <h2 className="show-title">{this.props.picture.title}</h2>
+            <div className="button-holder">
+              <button className="button1">Edit</button>
+              <button className="button2">Delete</button>
+            </div>
 
+        </div>
       </div>
     );
   }
