871de45d040922c34207e90f2200fc76e3470202
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-10 11:31:42 -0700

    finished show page follow button

--- frontend/components/show/show_picture.jsx
+++ frontend/components/show/show_picture.jsx
@@ -32,6 +32,28 @@ class ShowPictureComponent extends React.Component {
     this.props.openModal({string:'edit', picture:this.props.picture});
   }
 
+  updateFollow(){
+    const {deleteFollow, createFollow, follow, user, currentUser} = this.props;
+    if (follow){
+      deleteFollow(follow.id);
+    } else {
+      createFollow({leader_id: user.id, follower_id: currentUser.id});
+    }
+  }
+
+  followButton() {
+    const {follow,currentUser,user} = this.props;
+    if (follow && currentUser.id !== user.id){
+      return (
+        <button onClick={() => this.updateFollow()} className="show-following"><span>Following</span></button>
+      );
+    } else if (currentUser.id !== user.id) {
+      return (
+        <button onClick={() =>this.updateFollow()} className="show-follow">Follow</button>
+      );
+    }
+  }
+
   render() {
     return (
       <div className="show-div">
@@ -39,8 +61,9 @@ class ShowPictureComponent extends React.Component {
         <div className="show-child-1"><img className='show-image' src={`${this.props.picture.image_url}`}/></div>
           <div className="show-child-2">
             <p> <img className = "image-placeholder" src="https://s15.postimg.cc/8agnin31n/seth_curry.jpg" />
-            <span className="show-username"><Link className={`show-link ${this.nameLink()}`} to={`/profile/${this.props.user.id}`} >{this.props.user.username}</Link></span></p>
-            <h2 className="show-title">{this.props.picture.title}</h2>
+            <span className="show-username"><Link className={`show-link ${this.nameLink()}`} to={`/profile/${this.props.user.id}`} >{this.props.user.username}</Link></span>{this.followButton()}</p>
+
+            <h1 className="show-title">{this.props.picture.title}</h1>
             <div className="button-holder">
               {this.buttonDisplay()}
             </div>
--- frontend/components/show/show_picture_container.js
+++ frontend/components/show/show_picture_container.js
@@ -1,21 +1,27 @@
 import { connect } from 'react-redux';
 import React from 'react';
+import {findFollow} from '../../util/selectors';
 import ShowPictureComponent from './show_picture';
 import { closeModal, openModal } from '../../actions/modal_actions';
 import {deletePicture, updatePicture} from '../../actions/picture_actions';
+import {fetchFollows, createFollow, deleteFollow} from '../../actions/follow_actions';
 
-const mapStateToProps = ({session, entities:{users},ui:{modal}}) => (
+const mapStateToProps = ({follows, session, entities:{users},ui:{modal}}) => (
    {
      currentUser: users[session.id],
      picture: modal.picture,
-     user: modal.user
+     user: modal.user,
+     follow: findFollow(follows,session.id, parseInt(modal.picture.uploader_id))
    }
 );
 const mapDispatchToProps = () => dispatch => ({
   closeModal: () => dispatch(closeModal()),
   deletePicture: (photoId) => dispatch(deletePicture(photoId)),
   updatePicture: (photo) => dispatch(updatePicture(photo)),
-  openModal: (object) => dispatch(openModal(object))
+  openModal: (object) => dispatch(openModal(object)),
+  fetchFollows: (follows) => dispatch(fetchFollows(follows)),
+  createFollow: (follow) => dispatch(createFollow(follow)),
+  deleteFollow: (id) => dispatch(deleteFollow(id))
 
 });
 
