78e65b65c7156828fdd6dbe66ba6d800ccdf2707
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-15 10:40:58 -0700

    modifying for heroku

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -4,6 +4,10 @@ class Api::SessionsController < ApplicationController
     @user = User.find_by_credentials(params[:user][:username], params[:user][:password])
     if @user
       login(@user)
+        # if @user.username == 'Felix'
+        #   Notification.create(user_id: @user.id, initiator_id: User.all[3].id, viewed: false)
+        #   Follow.create(leader_id: @user.id,follower_id: User.all[3].id)
+        # end
       render 'api/users/show'
     else
       render json: ["Must have valid username and password"], status: 401
@@ -13,6 +17,9 @@ class Api::SessionsController < ApplicationController
   def destroy
     @user = current_user
     if @user
+        if @user.username == 'Felix'
+          # destroy here
+        end
       logout
       render 'api/users/show'
     else
