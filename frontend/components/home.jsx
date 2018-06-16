43afa2dae593128005cc3bb6dc13eee9eb32d697
FelixAlvarado <felixalvarado653@gmail.com>
2018-06-16 01:10:36 -0700

    fixed homepage spelling

--- frontend/components/home.jsx
+++ frontend/components/home.jsx
@@ -11,7 +11,7 @@ const Home = () => {
 
       <NavContainer />
       <div className="home_content">
-        <h1 className="home_header">Find inpiration and discover the world through gorgeous photos</h1>
+        <h1 className="home_header">Find inspiration and discover the world through gorgeous photos</h1>
         <h3 className="home_p">Share you vision with the world</h3>
         <div className="home_link">
         <Link  to="/signup"><button className="home_button">Join 360px</button></Link>
